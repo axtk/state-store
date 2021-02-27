59db0c83ca281a87c2c60d1a60864ed3baaf873d
axtk <axtkx@inbox.ru>
2021-02-27 07:22:04 +0300

    added revision

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ class Store {
     constructor(initialState) {
         this.state = {};
         this.eventManager = new EventManager();
+        this.revision = 0;
 
         if (initialState != null)
             this.setState(initialState);
@@ -22,6 +23,7 @@ class Store {
             throw new Error('handler is not a function');
 
         let listener = this.eventManager.addListener(StoreEvent.UPDATE, () => {
+            this.revision = this.revision === Number.MAX_SAFE_INTEGER ? 0 : this.revision + 1;
             handler(this.getState());
         });
 
@@ -57,6 +59,9 @@ class Store {
         unset(this.state, path);
         this.state = updateBranch(this.state, path);
         this.eventManager.dispatch(StoreEvent.UPDATE);
+    }
+    getRevision() {
+        return this.revision;
     }
 }
 
