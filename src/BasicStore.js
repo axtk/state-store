f5af69e6cb86fe5b822e7d01e18d0f2538d8cbcd
axtk <axtkx@inbox.ru>
2021-03-17 23:15:31 +0300

    update handler for revision update

--- src/BasicStore.js
+++ src/BasicStore.js
@@ -7,13 +7,16 @@ class BasicStore {
         this.eventManager = new EventManager();
         this.revision = 0;
         this.setState(initialState);
+
+        this.eventManager.addListener(UPDATE_EVENT, () => {
+            this.revision = this.revision === Number.MAX_SAFE_INTEGER ? 1 : this.revision + 1;
+        });
     }
     onUpdate(handler) {
         if (typeof handler !== 'function')
             throw new Error('handler is not a function');
 
         let listener = this.eventManager.addListener(UPDATE_EVENT, () => {
-            this.revision = this.revision === Number.MAX_SAFE_INTEGER ? 1 : this.revision + 1;
             handler(this);
         });
 
