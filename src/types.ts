b9eda5fb8076c92dc82e0bf3c473b020d292276f
axtk <axtkx@inbox.ru>
2021-04-01 23:19:30 +0300

    rm PathComponent type

--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,4 @@ import type {Map as ImmutableMap} from 'immutable';
 
 export type State = any;
 export type ImmutableState = ImmutableMap<any, any>;
-
-export type PathComponent = string | number;
-export type Path = string | PathComponent[];
+export type Path = string | Array<string | number>;
